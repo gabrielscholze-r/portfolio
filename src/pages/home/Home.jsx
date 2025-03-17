aa8fa5820590b5007154b3b59c5fa2b8a1306b0b
gabrielscholze-r <gabrielscholze24@gmail.com>
2025-03-17 16:27:19 -0300

    colors

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
           Sou desenvolvedor full-stack com cerca de três anos de experiência, atuando principalmente com C# (.NET) e React.js. Atualmente, trabalho na manutenção e evolução de um projeto para clientes nos Estados Unidos. Sou formado em Ciência da Computação (2024) e estou cursando uma pós-graduação em Desenvolvimento de Aplicativos Móveis para expandir meus conhecimentos e aprofundar minha atuação no mercado de tecnologia
         </p>
       </div>
-      <hr className='mx-5' />
+      <hr className='mx-5' style={{color: "var(--text-color)"}}/>
       <div>
         <div className="row techs text-center w-75 p-5 mx-auto justify-content-center">
           <h2 className='pt-2 pb-5 techs-title'>Skills</h2>
