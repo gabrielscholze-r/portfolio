44acab4fb831b1bd6609a3134b0e364b1cef121a
gabrielscholze-r <gabrielscholze24@gmail.com>
2025-03-28 13:58:10 -0300

    🔧

--- src/components/home/detailItem/DetailItem.jsx
+++ src/components/home/detailItem/DetailItem.jsx
@@ -6,13 +6,13 @@ export default function DetailItem({ title, date, place, description }) {
         <div className='detailitem px-3 py-1 rounded'>
             <div className='d-flex flex-column text-left pb-3 pt-2 w-100'>
                 <div className='text-date'>
-                    <div className='title-detail-item'>
-                        <h3 className='title'>{title}</h3>
+                    <div className='title-detail-item text-left'>
+                        <h3 className='title mx-0 px-0'>{title}</h3>
                         <h4 className='place rounded'>{place}</h4>
                     </div>
                     <p className='my-auto date'>{date}</p>
                 </div>
-                <p className='description'>{description}</p>
+                <p className='description mx-0 px-0'>{description}</p>
             </div>
         </div>
     )
