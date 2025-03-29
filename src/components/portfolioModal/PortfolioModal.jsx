24c7e2d88cc4573c7fe6cf2a702980e27aa3d45f
gabrielscholze-r <gabrielscholze24@gmail.com>
2025-03-29 17:06:26 -0300

    color fix

--- src/components/portfolioModal/PortfolioModal.jsx
+++ src/components/portfolioModal/PortfolioModal.jsx
@@ -9,7 +9,7 @@ export default function PortfolioModal({ info, close }) {
             </div>
             <div className='info-content'>
                 <div className="sector-portfolio my-3">Este portfólio foi desenvolvido utilizando React.JS</div>
-                <div className="sector-portfolio2 my-3">Para mais informações sobre projeto, acese o github abaixo</div>
+                <div className="sector-portfolio2 my-3">Para mais informações sobre projeto, acesse o github abaixo</div>
                 <a className='project-link-info my-3' href="https://github.com/gabrielscholze-r/portfolio" target='_blank'>
                     <i className="bi bi-github github-info"></i>
                 </a>
