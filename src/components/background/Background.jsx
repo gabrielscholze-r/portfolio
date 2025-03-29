68b81a176172e5da5265479107b4169d70e02182
gabrielscholze-r <gabrielscholze24@gmail.com>
2025-03-29 14:31:36 -0300

    intro and background

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import './Background.css'
+import './Background.css';
 
-const getRandomPosition = () => Math.random() * 95 + '%';
-const getRandomDelay = () => Math.random() * 2; 
+const getRandomPosition = () => Math.random() * 100 + '%'; // Posição aleatória das linhas
+const getRandomDelay = () => Math.random() * 5; // Delay aleatório para animações
 
 export default function Background() {
   return (
     <div className="geometric-background">
       {[...Array(3)].map((_, i) => (
-        <div 
+        <div
           key={i}
-          className="geometric-shape square" 
+          className="wave"
+          style={{
+            animationDelay: `${getRandomDelay()}s`,
+            animationDuration: `${15 + Math.random() * 15}s`,
+          }}
+        ></div>
+      ))}
+      
+      {[...Array(4)].map((_, i) => (
+        <div
+          key={i}
+          className="line"
           style={{
             left: getRandomPosition(),
             animationDelay: `${getRandomDelay()}s`,
-            animationDuration: `${30 + Math.random() * 30}s`,
+            animationDuration: `${20 + Math.random() * 20}s`,
           }}
         ></div>
       ))}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,31 +4,41 @@ import Skills from '../../components/home/skills/Skills'
 import Professional from '../../components/home/professional/Professional'
 import Education from '../../components/home/education/Education'
 import Certifications from '../../components/home/certifications/Certifications'
+import me from '../../assets/img/gabrielscholzerosa.png'
 export default function Home() {
 
 
   return (
     <div>
       <div className="intro text-center w-100 mx-auto">
-        <h2 className='name py-4'>
-          Gabriel Scholze Rosa
-        </h2>
-        <h3 className='job py-4'>
-          Programador Fullstack
-        </h3>
-        <p className='bio py-4'>
-          Sou desenvolvedor full-stack com cerca de três anos de experiência, atuando principalmente com C# (.NET) e React.js. Atualmente, trabalho na manutenção e evolução de um projeto para clientes nos Estados Unidos. Sou formado em Ciência da Computação (2024) e estou cursando uma pós-graduação em Desenvolvimento de Aplicativos Móveis para expandir meus conhecimentos e aprofundar minha atuação no mercado de tecnologia
-        </p>
+        <div className="d-flex justify-content-between w-100 align-items-center introduction-div">
+          <div className='px-5'>
+            <img src={me} alt="Gabriel Scholze Rosa" className='profile-pic mx-auto'/>
+          </div>
+
+          <div className='infos'>
+            <h2 className='name py-4'>
+              Gabriel Scholze Rosa
+            </h2>
+            <h3 className='job py-4'>
+              Programador Fullstack
+            </h3>
+            <p className='bio py-4'>
+              Sou desenvolvedor full-stack com cerca de três anos de experiência, atuando principalmente com C# (.NET) e React.js. Atualmente, trabalho na manutenção e evolução de um projeto para clientes nos Estados Unidos. Sou formado em Ciência da Computação (2024) e estou cursando uma pós-graduação em Desenvolvimento de Aplicativos Móveis para expandir meus conhecimentos e aprofundar minha atuação no mercado de tecnologia
+            </p>
+          </div>
+
+        </div>
       </div>
       <hr className='mx-5' style={{ color: "var(--text-color)" }} />
       <Skills />
       <hr className='mx-5' style={{ color: "var(--text-color)" }} />
-      
+
       <Professional />
       <hr className='mx-5' style={{ color: "var(--text-color)" }} />
-      <Education/>
+      <Education />
       <hr className='mx-5' style={{ color: "var(--text-color)" }} />
-      <Certifications/>
+      <Certifications />
     </div >
   )
 }
