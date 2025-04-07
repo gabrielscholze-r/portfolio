8b40742e7177590e02d4dc645de4fdf3c92883b4
gabrielscholze-r <gabrielscholze24@gmail.com>
2025-04-07 12:36:30 -0300

    detailOrder

--- src/components/home/education/Education.jsx
+++ src/components/home/education/Education.jsx
@@ -7,9 +7,9 @@ export default function Education() {
       <div className="w-100 text-center pt-5">
         <h1 className='sector py-5'>Formação Acadêmica</h1>
         <div className="w-50 d-flex justify-content-evenly mx-auto align-items-center d-flex flex-column gap-2">
-          <DetailItem title={"Ensino Médio"} date={"2017 - 2019"} place={"Colégio Estadual do Paraná"} description={"Ensino Médio Regular"} />
-          <DetailItem title={"Ciência da Computação"} date={"2020 - 2023"} place={"PUC-PR"} description={"Bacharelado em Ciência da Computação"} />
           <DetailItem title={"Desenvolvimento de Aplicativos Móveis"} date={"Em andamento"} place={"PUC-PR"} description={"Pós-Graduação em Desenvolvimento de Aplicativos Móveis"} />
+          <DetailItem title={"Ciência da Computação"} date={"2020 - 2023"} place={"PUC-PR"} description={"Bacharelado em Ciência da Computação"} />
+          <DetailItem title={"Ensino Médio"} date={"2017 - 2019"} place={"Colégio Estadual do Paraná"} description={"Ensino Médio Regular"} />
         </div>
       </div>
 
--- src/components/home/professional/Professional.jsx
+++ src/components/home/professional/Professional.jsx
@@ -8,10 +8,10 @@ export default function Professional() {
         <div className="w-100 text-center pt-5">
             <h1 className='sector-profession py-5'>Experiência Profissional</h1>
             <div className="w-50 d-flex justify-content-evenly mx-auto align-items-center d-flex flex-column gap-2">
-                <DetailItem title={"Estagiário de Administração"} date={"01/2019 - 12/2019"} place={"Tribunal de Justiça do Paraná"} description={"Gerenciamento de inventário, comunicação entre setores e emissão de ofícios."} />
-                <DetailItem title={"Estagiário Backend"} date={"06/2022 - 02/2023"} place={"Celepar"} description={"Desenvolvimento Backend em Java (JSP)"} />
-                <DetailItem title={"Estagiário Fullstack"} date={"03/2023 - 02/2024"} place={"Sumersoft"} description={"Estágio em Desenvolvimento Fullstack .NET e React.js"} />
                 <DetailItem title={"Desenvolvedor Fullstack"} date={"03/2024 - Atualmente"} place={"Sumersoft"} description={"Desenvolvimento Fullstack .NET e React.js"} />
+                <DetailItem title={"Estagiário Fullstack"} date={"03/2023 - 02/2024"} place={"Sumersoft"} description={"Estágio em Desenvolvimento Fullstack .NET e React.js"} />
+                <DetailItem title={"Estagiário Backend"} date={"06/2022 - 02/2023"} place={"Celepar"} description={"Desenvolvimento Backend em Java (JSP)"} />
+                <DetailItem title={"Estagiário de Administração"} date={"01/2019 - 12/2019"} place={"Tribunal de Justiça do Paraná"} description={"Gerenciamento de inventário, comunicação entre setores e emissão de ofícios."} />
             </div>
         </div>
     )
