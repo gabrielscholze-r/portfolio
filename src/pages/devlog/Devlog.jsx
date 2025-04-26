bef028e9d929dd732ae82dece044766daf95c3e5
gabrielscholze-r <gabrielscholze24@gmail.com>
2025-04-25 22:33:23 -0300

    filtro

--- src/pages/devlog/Devlog.jsx
+++ src/pages/devlog/Devlog.jsx
@@ -1,23 +1,45 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import data from "../../assets/devlog/devlog.json";
 import DevlogItem from "../../components/devlog/devlogItem/DevlogItem";
-import './Devlog'
 
 export default function Devlog() {
-  const [anyOpen, setAny] = useState(false)
+    const [anyOpen, setAny] = useState(false);
+    const [filter, setFilter] = useState("");
 
-  return (
-    <section className="mt-5">
-      <h1 className="text-center text-title-color display-4">Devlog</h1>
-      <div className="mt-3">
+    const handleChange = (e) => {
+        setFilter(e.target.value);
+    };
 
-        {data.map((v, i) => (
-          <div className="my-3">
-            <hr className="w-50 mx-auto text-color border-3"/>
-            <DevlogItem data={v} key={i} anyOpen={anyOpen} setAny={setAny}/>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+    const filteredData = filter
+        ? data.filter((item) =>
+            item.tags.some((tag) => tag.toLowerCase().includes(filter.toLowerCase()))
+        )
+        : data;
+
+    return (
+        <section className="mt-5">
+            <h1 className="text-center text-title-color display-4">Devlog</h1>
+
+            <select
+                name="category"
+                id="category"
+                value={filter}
+                onChange={handleChange}
+                className="form-select w-auto mx-3 shadow-custom text-title-color mx-auto my-3"
+            >
+                <option value="" disabled>Filtrar</option>
+                <option value="">Todos</option>
+                <option value="Portfolio">Portfolio</option>
+                <option value="Outros projetos">Outros projetos</option>
+            </select>
+            <div className="mt-3">
+                {filteredData.map((v, i) => (
+                    <div className="my-3" key={i}>
+                        <hr className="w-50 mx-auto text-color border-3"/>
+                        <DevlogItem data={v} anyOpen={anyOpen} setAny={setAny}/>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 }
